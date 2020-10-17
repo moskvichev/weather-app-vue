f62558a595d6019f118420e8962f452adeddfab8
moskvicheveg <moskvicheveg@gmail.com>
2020-10-18 01:22:33 +0400

    city api

---
+++ src/components/City.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="period">{{city_data}}</div>
+</template>
+
+<script>
+export default {
+    name: "City",
+    props: {
+        city_data: {
+            type: Object           
+        }
+    },
+    data() {
+        return {}
+    },
+    computed: {}
+}
+
+</script>
